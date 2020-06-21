a3c692599a99291350abda88e0334737786d5b6a
Travis Fox <travis.fox90@gmail.com>
2020-06-21 02:43:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,8 @@ t_minus = 10
 
 def countdown(t_minus)
   while t_minus > 0
-    
+    t_minus -= 1
     puts "#{t_minus} SECOND(S)!"
-
   end
   return "HAPPY NEW YEAR!"
 end
@@ -12,8 +11,8 @@ end
 
 def countdown_with_sleep(t_minus)
   while t_minus > 0
-    puts "#{t_minus} SECOND(S)!"
     t_minus -= 1
+    puts "#{t_minus} SECOND(S)!"
     sleep(1)
   end
   return "HAPPY NEW YEAR!"
